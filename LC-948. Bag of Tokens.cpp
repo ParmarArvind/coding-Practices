ba36f5d8ca5728c6496a79fb6cd2d69e56618551
Arvind Parmar <arvind1057956@gmail.com>
2026-02-26 21:34:38 +0530

    Fix formatting issues in Bag of Tokens.cpp

---
+++ LC-948. Bag of Tokens.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public: 
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(),tokens.end());
+        int i=0;
+        int j=tokens.size()-1;
+        int score=0;
+        int maxScore=0;
+        while(i<=j)
+        {
+            if(power>=tokens[i])
+            {
+                power -=tokens[i];
+                score++;
+                maxScore = max(maxScore,score);
+                i++;
+            }
+            else if(score>0)
+            {
+                power +=tokens[j];
+                score--;
+                j--;
+            }
+            else break;
+        }
+
+        return maxScore;
+    }
+
+};
