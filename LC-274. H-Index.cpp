f53d505a0eadd5c9a8457dfd08592d7305f9b0ce
Arvind Parmar <arvind1057956@gmail.com>
2026-03-05 02:00:01 +0530

    Create LC-274. H-Index.cpp

---
+++ LC-274. H-Index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        int result=0;
+        int n=citations.size();
+        for(int i = 0 ; i<n ; i++)
+        {
+            if(citations[i] >=n-i)
+            {
+                return n-i;
+            }
+        }
+        return result;
+    }
+};
