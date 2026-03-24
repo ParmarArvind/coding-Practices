c991702469b2593089453e5a783544678a4418d4
Arvind Parmar <arvind1057956@gmail.com>
2026-03-24 11:55:09 +0530

    Update 07 Maximum Length of Pair Chain (LC- 464) .cpp

--- DP concepts/07 Maximum Length of Pair Chain (LC- 464) .cpp
+++ DP concepts/07 Maximum Length of Pair Chain (LC- 464) .cpp
@@ -31,7 +31,34 @@ public:
     }
 };
 
-//method -2 greddy
+//method -2 bottom up
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        
+        int n=pairs.size();
+        sort(pairs.begin(), pairs.end());
+        vector<int> t(n,1);
+        
+        int maxLength=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(pairs[j][1] <pairs[i][0])
+                {
+                    t[i]=max(t[i],t[j]+1);
+                    maxLength= max(maxLength,t[i]);
+                }
+            }
+        }
+
+        return maxLength;
+
+    }
+};
+
+//method -3 greddy
 class Solution {
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
