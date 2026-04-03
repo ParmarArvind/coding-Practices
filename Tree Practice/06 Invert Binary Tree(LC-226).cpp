09084bc59ea8af4e741c6b79dbb638f582b1cc51
Arvind Parmar <arvind1057956@gmail.com>
2026-04-03 16:15:53 +0530

    Remove TreeNode definition comments from Invert Binary Tree
    Removed comments for the TreeNode structure definition.

---
+++ Tree Practice/06 Invert Binary Tree(LC-226).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void solve(TreeNode* root)
+    {
+        if(!root) return ;
+        solve(root->left);
+        solve(root->right);
+        TreeNode* temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+        return;
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        solve(root);
+
+        return root;
+    }
+};
