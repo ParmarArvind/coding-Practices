27d84ce21204d9dee6bd35f82c90d450d5bf3e30
Arvind Parmar <arvind1057956@gmail.com>
2026-04-03 16:08:39 +0530

    Create 05 Count Complete Tree Nodes(LC-222).cpp

---
+++ Tree Practice/05 Count Complete Tree Nodes(LC-222).cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int count(TreeNode* root)
+    {
+        if(!root) return 0;
+        return 1+count(root->left)+count(root->right);
+    }
+    int countNodes(TreeNode* root) {
+        return count(root);
+    }
+};
